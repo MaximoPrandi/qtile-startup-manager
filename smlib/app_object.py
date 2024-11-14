522296c7f7c68c14cb5c66497ffe3b72fa6b8027
MaximoPrandi <maximoprandi@gmail.com>
2024-11-13 21:33:05 -0300

    feat(app_object): add __repr__, add __init__, add path property, add load from applications directory

--- smlib/app_object.py
+++ smlib/app_object.py
@@ -8,11 +8,19 @@ class App:
     A class representing an user App.
 
     Attributes:
-        groups (list[Group]): The group or groups where the app belongs.
-        name (DesktopEntry): The app DesktopEntry instance.
+        app (DesktopEntry): The app DesktopEntry instance.
+        path (DesktopEntry): The DesktopEntry file path.
     """
     _app: DesktopEntry
-        
+    _path: str
+    
+    def __init__(self, path: str):
+        self.app = path
+        self.path = path
+    
+    def __repr__(self):
+        return self.path
+    
     def match(self) -> str:
         """
         Return the application match ready to be used by Qtile
@@ -41,15 +49,33 @@ class App:
         """
         return self._app
 
+    @property
+    def path(self) -> str:
+        """
+        Return the DesktopEntry instance with the app '.Desktop' information.
+
+        Returns:
+            DesktopEntry: A DesktopEntry instance.
+        """
+        return self._path
+
+    @path.setter
+    def path(self, dir: str):
+        self._path = dir
+
     @app.setter
     def app(self, dir: str):
         try:
-            if path.isfile(dir):
-                system(f"cp {dir} apps/{path.basename(str)}")
-                _desktop_entry = DesktopEntry(f"apps/{path.basename(str)}")
+            if path.isfile(f"applications/{path.basename(dir)}"):
+                _desktop_entry = DesktopEntry(f"applications/{path.basename(dir)}")
                 if _desktop_entry.getName:
-                    self._app = DesktopEntry(f"apps/{path.basename(str)}")
+                    self._app = DesktopEntry(f"applications/{path.basename(dir)}")
+            elif path.isfile(dir):
+                system(f"cp {dir} applications/{path.basename(dir)}")
+                _desktop_entry = DesktopEntry(f"applications/{path.basename(dir)}")
+                if _desktop_entry.getName:
+                    self._app = DesktopEntry(f"applications/{path.basename(dir)}")
             else:
-                raise ValueError("Invalid application path")
-        except Exception:
+                raise ValueError(f"Invalid application path '{dir}'")
+        except Exception as error:
             raise Exception
