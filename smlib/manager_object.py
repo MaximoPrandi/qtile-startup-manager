d94d2515ae4c617b7a261b84f3f0e0ea5774599e
MaximoPrandi <maximoprandi@gmail.com>
2024-11-13 22:08:30 -0300

    feat(manager_object): add load_manager, add initialize, add __iter__

--- smlib/manager_object.py
+++ smlib/manager_object.py
@@ -1,6 +1,10 @@
-from config import APP_ROUTES, FONTS, PROMPT_ON_STARTUP, DEFAULT_ENVIRONMENT
+from config import APP_ROUTES, FONTS, PROMPT_ON_STARTUP, DEFAULT_ENVIRONMENT, MANUAL_SETUP, MANUAL_DATA
 from dataclasses import dataclass, field
+from json import dump, load, dumps
 from environment_object import Environment
+from group_object import Group
+from app_object import App
+from copy import deepcopy
 
 @dataclass
 class Manager:
@@ -10,4 +14,26 @@ class Manager:
     Attributes:
         environments (list[Environment]): The list of environments the manager has.
     """
-    _environments: list[Environment] = field(default_factory=list)
+    environments: list[Environment] = field(default_factory=list)
+    
+    def load_manager(self, env_list: list):
+        for env_data in env_list:
+            _Groups = []
+            for group_data in env_data["groups"]:
+                _Apps = []
+                for app_data in group_data["applications"]:
+                    _Apps.append(App(app_data))
+                group_data["applications"] = _Apps
+                _Groups.append(Group(**group_data))
+            env_data["groups"] = _Groups
+            self.environments.append(Environment(**env_data))
+    
+    def initialize(self, json: bool = False):
+        if json:
+            with open("json/data.json") as file:
+                self.load_manager(load(file))
+        elif MANUAL_SETUP:
+            self.load_manager(deepcopy(MANUAL_DATA))
+    
+    def __iter__(self):
+        return [environment.__dict__() for environment in self.environments]
