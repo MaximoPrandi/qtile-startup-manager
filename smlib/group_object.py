95f31ec6483c527b2aa225aefe502db641e79367
MaximoPrandi <maximoprandi@gmail.com>
2024-11-13 22:07:16 -0300

    feat(group_object): dataclass convert, add qtile_dict, add name default, modify __dict__, remove __del__

--- smlib/group_object.py
+++ smlib/group_object.py
@@ -15,16 +15,12 @@ class Group:
         exclusive (bool): If the group applications spawn on startup.
     """
     applications: list[App] = field(default_factory=list)
-    name: str
+    name: str = "Default"
     label: str = "\ueea8"
     exclusive: bool = False
     spawn: bool = True
-
-    def __del__(self):
-        for application in self._applications:
-            del(application.groups, [self])
     
-    def __dict__(self) -> dict:
+    def qtile_dict(self):
         """
         Return the group info ready to be used by Qtile
 
@@ -34,99 +30,16 @@ class Group:
         _info = {
             "name":self,
             "label":self.label,
-            "matches":map(App.match,self.applications),
+            "matches":map(App.match, self.applications),
             "exclusive":self.exclusive,
-            "spawn":map(App.spawn,self.applications)
+            "spawn":map(App.spawn, self.applications)
         }
-
-    @property
-    def applications(self) -> list[App]:
-        """
-        Return a list of the group applications
-
-        Returns:
-            list[App]: A list of the group applications.
-        """
-        return self._applications
-
-    @property
-    def name(self) -> str:
-        """
-        Return the group name
-
-        Returns:
-            str : The group name
-        """
-        return self._name
     
-    @property
-    def label(self) -> str:
-        """
-        Return the group label, the UTF16 code of an icon, used for example in GroupBox()
-
-        Returns:
-            str : The group label
-        """
-        return self._label
-
-    @property
-    def exclusive(self) -> bool:
-        """
-        Return if the app or applications are of exclusive use in the group
-
-        Returns:
-            bool : Exclusive boolean
-        """
-        return self._exclusive
-    
-    @property
-    def spawn(self) -> bool:
-        """
-        Return if the app or applications are spawned on startup
-
-        Returns:
-            str : Spawn boolean
-        """
-        return self._spawn
-    
-    @applications.setter
-    def applications(self, applications: list[App], replace: bool = False):
-        if any(app in self._applications for app in applications):
-            raise ValueError("There's already at least one of the list app\'s in the group")
-        self._applications = self._applications + applications
-    
-    @name.setter
-    def name(self, name: str):
-            self._name = name
-            
-    @label.setter
-    def label(self, label: str):
-        self._label = label
-            
-    @exclusive.setter
-    def exclusive(self, exclusive: bool):
-        self._exclusive = exclusive
-            
-    @spawn.setter
-    def spawn(self, spawn: bool):
-        self._spawn = spawn
-        
-    @applications.deleter
-    def applications(self, applications: list[App]):
-        self._applications = filter(lambda x: (x not in applications) ,self._applications)
-
-    @name.deleter
-    def name(self):
-        self._name = f"Default {self.environment.free_group_number()}"
-            
-    @label.deleter
-    def label(self):
-        self._label = "\ueea8"
-            
-    @exclusive.deleter
-    def exclusive(self):
-        self._exclusive = False
-            
-    @spawn.deleter
-    def spawn(self):
-        self._spawn = False
+    def __dict__(self):
+        return {
+            "applications":[app.__repr__() for app in self.applications],
+            "name":self.name,
+            "label":self.label,
+            "exclusive":self.exclusive,
+            "spawn":self.spawn
+        }
